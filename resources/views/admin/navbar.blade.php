81b19e18bba1da92a12ca771afae5a8dfab90384
Tajul Islam <tajulislam42053@gmail.com>
2023-10-21 18:41:39 +0600

    added

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid page-body-wrapper">
-    <nav class="navbar p-0 fixed-top d-flex ">
+    <nav class="navbar p-0 fixed-top d-flex " >
         <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                 <span class="mdi mdi-menu"></span>
@@ -11,6 +11,12 @@
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </li>
+                <li>
+                    <div style="padding: 50px">
+                        <a href="{{'chatify'}}">Message</a>
+                    </div>
+                </li>
+
                 <li>
                     <x-app-layout>
 
